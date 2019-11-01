31727c6e5f0d319980ed0931ef643862c2699233
huangk4 <847209149@qq.com>
2019-11-01 14:26:17 +0800

    second

--- get_word.py
+++ get_word.py
@@ -3,240 +3,249 @@ import math
 
 import jieba
 import jieba.posseg as psg
-from gensim import corpora,models
+from gensim import corpora, models
 from jieba import analyse
 import functools
 
-#停用词表加载方法
+# 停用词表加载方法
 def get_stopword_list():
     # 停用词表存储路径，每一行为一个词，按行读取进行加载
     # 进行编码转换确保匹配准确率
-    stop_word_path='./stopword.txt'
-    stopword_list=[sw.replace('\n','') for sw in open(stop_word_path,encoding='utf-8').readlines()]
+    stop_word_path = './stopword.txt'
+    stopword_list = [sw.replace('\n', '') for sw in open(stop_word_path,encoding='utf-8').readlines()]
     return stopword_list
 
-#分词方法，调用jieba
-def seg_to_list(sentence,pos=False):
+
+# 分词方法，调用结巴接口
+def seg_to_list(sentence, pos=False):
     if not pos:
         # 不进行词性标注的分词方法
-        seg_list=jieba.cut(sentence)
+        seg_list = jieba.cut(sentence)
     else:
         # 进行词性标注的分词方法
-        seg_list=psg.cut(sentence)
+        seg_list = psg.cut(sentence)
     return seg_list
 
-#去除干扰词
-def word_filter(seg_list,pos=False):
-    stopword_list=get_stopword_list()
-    filter_list=[]
-    #根据pos参数选择是否词性过滤
-    #不进行词性过滤，则将词性都标记为n，表示全部保留
+
+# 去除干扰词
+def word_filter(seg_list, pos=False):
+    stopword_list = get_stopword_list()
+    filter_list = []
+    # 根据POS参数选择是否词性过滤
+    ## 不进行词性过滤，则将词性都标记为n，表示全部保留
     for seg in seg_list:
         if not pos:
-            word=seg
-            flag='n'
+            word = seg
+            flag = 'n'
         else:
-            word=seg.word
-            flag=seg.flag
+            word = seg.word
+            flag = seg.flag
         if not flag.startswith('n'):
             continue
-        #过滤高停用词表中的词，以及长度<2的词
-        if not word in stopword_list and len(word)>1:
+        # 过滤停用词表中的词，以及长度为<2的词
+        if not word in stopword_list and len(word) > 1:
             filter_list.append(word)
+
     return filter_list
 
-#数据加载，返回进行分词和过滤后的数据，pos为是否词性标注的参数，corpus_path为数据集路径
-def load_data(pos=False,corpus_path='./corpus.txt',encoding='utf-8'):
-    #调用上面方式为数据集处理，处理后的每条数据仅保留非干扰词
-    doc_list=[]
-    for line in open(corpus_path,'r',encoding='utf-8'):
-        content=line.strip()
-        seg_list=seg_to_list(content,pos)
-        filter_list=word_filter(seg_list,pos)
+
+# 数据加载，pos为是否词性标注的参数，corpus_path为数据集路径
+def load_data(pos=False, corpus_path='./corpus.txt'):
+    # 调用上面方式对数据集进行处理，处理后的每条数据仅保留非干扰词
+    doc_list = []
+    for line in open(corpus_path, 'r',encoding='utf-8'):
+        content = line.strip()
+        seg_list = seg_to_list(content, pos)
+        filter_list = word_filter(seg_list, pos)
         doc_list.append(filter_list)
-    
+
     return doc_list
 
-#IDF值统计方法
-def train_idf(doc_list):
-    idf_dic={}
-    #总文档数
-    tt_count=len(doc_list)
 
-    #每个词出现的文档数
+# idf值统计方法
+def train_idf(doc_list):
+    idf_dic = {}
+    # 总文档数
+    tt_count = len(doc_list)
+
+    # 每个词出现的文档数
     for doc in doc_list:
         for word in set(doc):
-            idf_dic[word]=idf_dic.get(word,0.0)+1.0
+            idf_dic[word] = idf_dic.get(word, 0.0) + 1.0
 
-    #按公式转换为idf值，分母加1进行平滑处理
-    for k,v in idf_dic.items():
-        idf_dic[k]=math.log(tt_count/(1.0+v))
-    #对于没有出现在字典的词，默认只在一个文档出现，得到默认值
-    default_idf=math.log(tt_count/(1.0))
-    return idf_dic,default_idf
+    # 按公式转换为idf值，分母加1进行平滑处理
+    for k, v in idf_dic.items():
+        idf_dic[k] = math.log(tt_count / (1.0 + v))
 
-def train_lsi(self):
-    lsi=models.LsiModel(self.corpus_tfidf,id2word=self.dictionary,num_topics=self.num_topics)
-    return lsi
-    
-def train_lda(self):
-    lda=models.LdaModel(self.corpus_tfidf,id2word=self.dictionary,num_topics=self.num_topics)
-    return lda
+    # 对于没有在字典中的词，默认其仅在一个文档出现，得到默认idf值
+    default_idf = math.log(tt_count / (1.0))
+    return idf_dic, default_idf
 
-#排序函数，用于topK关键词的按值排
-def cmp(e1,e2):
+
+#  排序函数，用于topK关键词的按值排序
+def cmp(e1, e2):
     import numpy as np
-    res=np.sign(e1[1]-e2[1])
-    if res!=0:
+    res = np.sign(e1[1] - e2[1])
+    if res != 0:
         return res
     else:
-        a=e1[0]+e2[0]
-        b=e2[0]+e1[0]
-        if a>b:
+        a = e1[0] + e2[0]
+        b = e2[0] + e1[0]
+        if a > b:
             return 1
-        elif a==b:
+        elif a == b:
             return 0
         else:
             return -1
 
-#TF-IDF类
+# TF-IDF类
 class TfIdf(object):
-    #四个参数为：训练好的idf字典，默认idf值，处理后的待提取文本，关键词数量
-    def __init__(self,idf_dic,default_idf,word_list,keyword_num):
-        self.word_list=word_list
-        self.idf_dic,self.default_idf=idf_dic,default_idf
-        self.tf_dic=self.get_tf_dic()
-        self.keyword_num=keyword_num
+    # 四个参数分别是：训练好的idf字典，默认idf值，处理后的待提取文本，关键词数量
+    def __init__(self, idf_dic, default_idf, word_list, keyword_num):
+        self.word_list = word_list
+        self.idf_dic, self.default_idf = idf_dic, default_idf
+        self.tf_dic = self.get_tf_dic()
+        self.keyword_num = keyword_num
 
-    #统计tf值
+    # 统计tf值
     def get_tf_dic(self):
-        tf_dic={}
+        tf_dic = {}
         for word in self.word_list:
-            tf_dic[word]=tf_dic.get(word,0.0)+1.0
-        tt_count=len(self.word_list)
-        for k,v in tf_dic.items():
-            tf_dic[k]=float(v)/tt_count
+            tf_dic[word] = tf_dic.get(word, 0.0) + 1.0
+
+        tt_count = len(self.word_list)
+        for k, v in tf_dic.items():
+            tf_dic[k] = float(v) / tt_count
+
         return tf_dic
 
-    #按公式计算tf-idf
+    # 按公式计算tf-idf
     def get_tfidf(self):
-        tfidf_dic={}
+        tfidf_dic = {}
         for word in self.word_list:
-            idf=self.idf_dic.get(word,self.default_idf)
-            tf=self.tf_dic.get(word,0)
+            idf = self.idf_dic.get(word, self.default_idf)
+            tf = self.tf_dic.get(word, 0)
 
-            tfidf=tf*idf
-            tfidf_dic[word]=tfidf
+            tfidf = tf * idf
+            tfidf_dic[word] = tfidf
 
-        #根据tf-idf排序，去排名前keyword_num的词作为关键词
-        for k,v in sorted(tfidf_dic.items(),key=functools.cmp_to_key(cmp),reverse=True)[:self.keyword_num]:
-            print(k+"/",end='')
+        tfidf_dic.items()
+        # 根据tf-idf排序，去排名前keyword_num的词作为关键词
+        for k, v in sorted(tfidf_dic.items(), key=functools.cmp_to_key(cmp), reverse=True)[:self.keyword_num]:
+            print(k + "/ ", end='')
         print()
 
-#主题模型
-class TopicModel(object):
-    #三个参数为：处理后的数据集，关键词数量，具体模型(LSI、LDA)，主题数量
-    def __init__(self,doc_list,keyword_num,model='LSI',num_topics=4):
-        #使用genism的接口，将文本转为向量化表示
-        #先构建词空间
-        self.dictionary=corpora.Dictionary(doc_list)
-        #使用BOW模型向量化
-        corpus=[self.dictionary.doc2bow(doc) for doc in doc_list]
-        #对每个词，根据tf-idf进行加权，得到加权后的向量表示
-        self.tfidf_model=models.TfidfModel(corpus)
-        self.corpus_tfidf=self.tfidf_model[corpus]
 
-        self.keyword_num=keyword_num
-        self.num_topics=num_topics
-        #选择加载的模型
-        if model=='LSI':
-            self.model=self.train_lsi()
+# 主题模型
+class TopicModel(object):
+    # 三个传入参数：处理后的数据集，关键词数量，具体模型（LSI、LDA），主题数量
+    def __init__(self, doc_list, keyword_num, model='LSI', num_topics=4):
+        # 使用gensim的接口，将文本转为向量化表示
+        # 先构建词空间
+        self.dictionary = corpora.Dictionary(doc_list)
+        # 使用BOW模型向量化
+        corpus = [self.dictionary.doc2bow(doc) for doc in doc_list]
+        # 对每个词，根据tf-idf进行加权，得到加权后的向量表示
+        self.tfidf_model = models.TfidfModel(corpus)
+        self.corpus_tfidf = self.tfidf_model[corpus]
+
+        self.keyword_num = keyword_num
+        self.num_topics = num_topics
+        # 选择加载的模型
+        if model == 'LSI':
+            self.model = self.train_lsi()
         else:
-            self.model=self.train_lda()
-        
-        #得到数据集的主题-词分布
+            self.model = self.train_lda()
+
+        # 得到数据集的主题-词分布
         word_dic = self.word_dictionary(doc_list)
-        self.wordtopic_dic=self.get_wordtopic(word_dic)
+        self.wordtopic_dic = self.get_wordtopic(word_dic)
 
     def train_lsi(self):
-        lsi=models.LsiModel(self.corpus_tfidf,id2word=self.dictionary,num_topics=self.num_topics)
+        lsi = models.LsiModel(self.corpus_tfidf, id2word=self.dictionary, num_topics=self.num_topics)
         return lsi
 
     def train_lda(self):
-        lda=models.LdaModel(self.corpus_tfidf,id2word=self.dictionary,num_topics=self.num_topics)
+        lda = models.LdaModel(self.corpus_tfidf, id2word=self.dictionary, num_topics=self.num_topics)
         return lda
 
-    def get_wordtopic(self,word_dic):
-        wordtopic_dic={}
+    def get_wordtopic(self, word_dic):
+        wordtopic_dic = {}
+
         for word in word_dic:
-            single_list=[word]
-            wordcorpus=self.tfidf_model[self.dictionary.doc2bow(single_list)]
-            wordtopic=self.model[wordcorpus]
-            wordtopic_dic[word]=wordtopic
+            single_list = [word]
+            wordcorpus = self.tfidf_model[self.dictionary.doc2bow(single_list)]
+            wordtopic = self.model[wordcorpus]
+            wordtopic_dic[word] = wordtopic
         return wordtopic_dic
 
-    #计算词的分布和文档的分布相似度，取相似度最高的keyword_num个词作为关键词
-    def get_simword(self,word_list):
-        sentcorpus=self.tfidf_model[self.dictionary.doc2bow(word_list)]
-        senttopic=self.model[sentcorpus]
-        #余弦相似度计算
-        def calsim(l1,l2):
-            a,b,c=0.0,0.0,0.0
-            for t1,t2 in zip(l1,l2):
-                x1=t1[1]
-                x2=t2[1]
-                a+=x1*x1
-                b+=x1*x1
-                c+=x2*x2
-            sim=a/math.sqrt(b*c) if not (b*c)==0.0 else 0.0
+    # 计算词的分布和文档的分布的相似度，取相似度最高的keyword_num个词作为关键词
+    def get_simword(self, word_list):
+        sentcorpus = self.tfidf_model[self.dictionary.doc2bow(word_list)]
+        senttopic = self.model[sentcorpus]
+
+        # 余弦相似度计算
+        def calsim(l1, l2):
+            a, b, c = 0.0, 0.0, 0.0
+            for t1, t2 in zip(l1, l2):
+                x1 = t1[1]
+                x2 = t2[1]
+                a += x1 * x1
+                b += x1 * x1
+                c += x2 * x2
+            sim = a / math.sqrt(b * c) if not (b * c) == 0.0 else 0.0
             return sim
 
-        #计算输入文本和每个词的主题分布相似度
-        sim_dic={}
-        for k,v in self.wordtopic_dic.items():
+        # 计算输入文本和每个词的主题分布相似度
+        sim_dic = {}
+        for k, v in self.wordtopic_dic.items():
             if k not in word_list:
                 continue
-            sim=calsim(v,senttopic)
-            sim_dic[k]=sim
+            sim = calsim(v, senttopic)
+            sim_dic[k] = sim
 
-            for k,v in sorted(sim_dic.items(),key=functools.cmp_to_key(cmp),reverse=True)[:self.keyword_num]:
-                print(k+"/",end='')
+        for k, v in sorted(sim_dic.items(), key=functools.cmp_to_key(cmp), reverse=True)[:self.keyword_num]:
+            print(k + "/ ", end='')
         print()
 
-    #词空间构建方法和向量化方法，在没有gensim接口时的一般处理方法
-    def word_dictionary(self,doc_list):
-        dictionary=[]
+    # 词空间构建方法和向量化方法，在没有gensim接口时的一般处理方法
+    def word_dictionary(self, doc_list):
+        dictionary = []
         for doc in doc_list:
             dictionary.extend(doc)
 
-        dictionary=list(set(dictionary))
+        dictionary = list(set(dictionary))
 
         return dictionary
-    
-    def doc2bowvec(self,word_list):
-        vec_list=[1 if word in word_list else 0 for word in self.dictionary]
+
+    def doc2bowvec(self, word_list):
+        vec_list = [1 if word in word_list else 0 for word in self.dictionary]
         return vec_list
 
-def tfidf_extract(word_list,pos=False,keyword_num=10):
-    doc_list=load_data(pos)
-    idf_dic,default_idf=train_idf(doc_list)
-    tfidf_model=TfIdf(idf_dic,default_idf,word_list,keyword_num)
+
+def tfidf_extract(word_list, pos=False, keyword_num=10):
+    doc_list = load_data(pos)
+    idf_dic, default_idf = train_idf(doc_list)
+    tfidf_model = TfIdf(idf_dic, default_idf, word_list, keyword_num)
     tfidf_model.get_tfidf()
 
-def textrank_extract(text,pos=False,keyword_num=10):
-    textrank=analyse.textrank
-    keywords=textrank(text,keyword_num)
-    #输出抽取的关键词
+
+def textrank_extract(text, pos=False, keyword_num=10):
+    textrank = analyse.textrank
+    keywords = textrank(text, keyword_num)
+    # 输出抽取出的关键词
     for keyword in keywords:
-        print(keyword+"/"),
+        print(keyword + "/ ", end='')
     print()
 
-def topic_extract(word_list,model,pos=False,keyword_num=10):
-    doc_list=load_data(pos)
-    topic_model=TopicModel(doc_list,keyword_num,model=model)
+
+def topic_extract(word_list, model, pos=False, keyword_num=10):
+    doc_list = load_data(pos)
+    topic_model = TopicModel(doc_list, keyword_num, model=model)
     topic_model.get_simword(word_list)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     text="该设备由浙江强脑科技有限公司推出，由孝顺镇中心小学校友捐赠给学校。学校建立了实验班“赋思学堂”，将“头环”用以实践。该小学在其官网6月29日发布的一则新闻中称，孝顺小学作为浙江省首个引进高端科技设备——“赋思头环”并应用于课堂教学、有效提高了课堂效率与学生专注力的学校，“受到了广泛关注”。\
 三个多月后，此事经媒体进一步报道，引发舆论热议。有声音认为，利用高科技头环“监控”学生上课是否认真，就如戴上了“紧箍咒”，无疑会让孩子们倍感压力，涉嫌侵犯使用者隐私。\
 10月31日下午，金东区教育局相关工作人员接受采访时称，“赋思学堂”并非“实验班”，而是“实验教室”；头环也并不是每天都要佩戴，而是一周一次。“学生是无偿、自愿使用头环，家长也对此表示认可，学生也未产生任何不良反应。”上述工作人员称。\
@@ -244,15 +253,16 @@ if __name__=='__main__':
 据其介绍，当地“乡贤”、杭州嘉银投资有限公司董事长孔小仙为回馈母校，于2018年9月向孝顺镇中心小学捐赠了50台“赋思头环”。据世界浙商网消息，孔小仙也是该款头环研发方——浙江强脑科技有限公司的天使投资人。\
 浙江强脑科技有限公司官网资料显示，其总部位于美国哈佛大学校园内，中国区总部则在浙江杭州。该公司称，其现拥有的独家技术涵盖硬件及软件方面，包括头环设备和电路板等硬件产品、基于专注力的核心算法及教育应用系统等。\
 10月31日，该公司在其官方微博连发两篇声明，表示“赋思头环”并非监控学生的产品，而是“训练学生提升注意力的仪器”。声明解释说，“赋思头环”利用神经反馈进行专注力训练；老师在课堂上也无法看到每个学生的专注力数值，只能看到平均数值。声明称，“赋思头环”收集的数据，不会发送给家长；且不需要长期佩戴，只需一周训练一次。"
-    pos=False
-    seg_list=seg_to_list(text,pos)
-    filter_list=word_filter(seg_list,pos)
 
-    print('TF-IDF结果：')
+    pos = True
+    seg_list = seg_to_list(text, pos)
+    filter_list = word_filter(seg_list, pos)
+
+    print('TF-IDF模型结果：')
     tfidf_extract(filter_list)
-    print('TextRank结果：')
+    print('TextRank模型结果：')
     textrank_extract(text)
-    print('LSI结果：')
-    topic_extract(filter_list,'LSI',pos)
-    print('LDA结果：')
-    topic_extract(filter_list,'LDA',pos)
+    print('LSI模型结果：')
+    topic_extract(filter_list, 'LSI', pos)
+    print('LDA模型结果：')
+    topic_extract(filter_list, 'LDA', pos)
